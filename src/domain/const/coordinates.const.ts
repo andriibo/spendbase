52331fe295d10a435f5b51fc82f765382ecc63c9
andrii <bondarenkoa1990@gmail.com>
2024-06-05 12:21:24 +0300

    const

---
+++ src/domain/const/coordinates.const.ts
@@ -0,0 +1,5 @@
+export const MinLat = -90;
+export const MaxLat = 90;
+
+export const MinLon = -180;
+export const MaxLon = 180;
--- src/presentation/views/requests/coordinates.request.ts
+++ src/presentation/views/requests/coordinates.request.ts
@@ -2,23 +2,24 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, Max, Min } from 'class-validator';
 import { CoordinatesRequestDto } from 'domain/dto/requests';
 import { Transform, TransformFnParams } from 'class-transformer';
+import { MaxLat, MaxLon, MinLat, MinLon } from 'domain/const';
 
 export class CoordinatesRequest extends CoordinatesRequestDto {
-  @ApiProperty()
+  @ApiProperty({ minimum: MinLat, maximum: MaxLat })
   @IsNotEmpty()
-  @Min(-90)
-  @Max(90)
+  @Min(MinLat)
+  @Max(MaxLat)
   @Transform(({ value }: TransformFnParams) => parseFloat(value))
   lat: number;
 
-  @ApiProperty()
+  @ApiProperty({ minimum: MinLon, maximum: MaxLon })
   @IsNotEmpty()
-  @Min(-180)
-  @Max(180)
+  @Min(MinLon)
+  @Max(MaxLon)
   @Transform(({ value }: TransformFnParams) => parseFloat(value))
   lon: number;
 
-  @ApiProperty()
+  @ApiProperty({ description: 'The part(s) that the application requests.' })
   @IsNotEmpty()
   @Transform(({ value }: TransformFnParams) => value.trim())
   part: string;
